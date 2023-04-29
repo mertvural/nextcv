5fbe5b610999de6cf711eefa5b29def3da5cc204
Mert Vural <mertvuraldev@hotmail.com>
2023-04-29 15:20:29 +0300

    edits

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="bg-black text-secondary px-2 py-2 text-sm">
+        <footer className="bg-black text-secondary px-2 py-2 text-xs">
             <div className="container">
-                <div className="flex items-center justify-between text-sm font-light">
+                <div className="flex items-center justify-between font-light">
                 2023 © All Right Reserved - NextCV        
                 <span>
                 Mert Vural
